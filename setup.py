d7c3d6c09fc41cfca6a5e57b6a5207bfe217c411
Pedro Dauer <81972735+Dauerrr@users.noreply.github.com>
2021-11-22 09:55:01 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='descritor_analise_sp',
+    version='0.0.1',
+
+    url='https://github.com/Dauerrr/Descritor-Analise-SP',
+    author='Pedro Dauer',
+    author_email='pedro.dauer2003@gmail.com',
+
+    py_modules=['descritor'],
+    install_requires=[
+    'pandas',
+],
+)
