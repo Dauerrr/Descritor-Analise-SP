a74b4b9340ca0c1eac825110753194fe5c7a95d2
Pedro Dauer <81972735+Dauerrr@users.noreply.github.com>
2021-12-02 12:10:44 -0300

    Update descritor.py

--- descritor.py
+++ descritor.py
@@ -1,6 +1,10 @@
-__version__ = '0.0.3'
+__version__ = '0.0.4'
+
 import sys
+from datetime import datetime
 import pandas as pd
+import requests
+import io
 
 
 class DescritorTabela:
@@ -8,8 +12,12 @@ class DescritorTabela:
     Construtor
     """
 
-    def __init__(self, caminho_arq, sep='\t'):
-        self.df = pd.read_csv(caminho_arq, sep=sep)
+    def __init__(self):
+        # self.df = pd.read_csv(caminho_arq, sep=sep)
+        self.min_int = -sys.maxsize - 1
+        self.max_int = sys.maxsize - 1
+
+    def preparar_df(self):
         self.df.columns = ['Data', 'Temp_Media', 'Temp_Min', 'Temp_Max', 'Precipitacao', 'Final_de_semana',
                            'Consumo_em_litros']
         self.df["Data"] = pd.to_datetime(self.df["Data"])
@@ -17,22 +25,22 @@ class DescritorTabela:
                                                                                                         "Temp_Min",
                                                                                                         "Temp_Max",
                                                                                                         "Precipitacao",
-                                                                                                        "Consumo_em_litros"]].apply(pd.to_numeric)
+                                                                                                        "Consumo_em_litros"]].apply(
+            pd.to_numeric)
         self.data = self.df.Data
         self.temp_max = self.df.Temp_Max
         self.temp_min = self.df.Temp_Min
         self.temp_media = self.df.Temp_Media
         self.precipitacao = self.df.Precipitacao
         self.consumo = self.df.Consumo_em_litros
-        self.min_int = -sys.maxsize - 1
-        self.max_int = sys.maxsize - 1
 
-    def pesquisar_por_data(self):
+    def pesquisar_por_data(self, data_especifica):
         """
         Função pesquisa data específica que o usuário solicitar.
+        Args:
+            -data_especifica: Data especifica que o usuario quer pesquisar
         """
         col_data = self.df['Data']
-        data_especifica = input("Escreva a data:(Ano-Mês-Dia) ")
         resultado = self.df.loc[col_data == data_especifica]
         return resultado
 
@@ -40,46 +48,43 @@ class DescritorTabela:
         """
         Função filtra dados da maior temperatura do ano e sua data.
         """
-        maior_temp = self.min_int
+        maior_temperatura = self.min_int
         maior_data = None
 
         for i in range(len(self.df)):
-            if maior_temp < self.df.iloc[i]['Temp_Max']:
-                maior_temp = self.df.iloc[i]['Temp_Max']
+            if maior_temperatura < self.df.iloc[i]['Temp_Max']:
+                maior_temperatura = self.df.iloc[i]['Temp_Max']
                 maior_data = self.df.iloc[i]['Data']
 
-        resposta = f"O dia {maior_data} com maior temperatura teve um total de {maior_temp} ºC."
-        return resposta
+        return maior_temperatura, maior_data
 
     def obter_data_temp_min(self):
         """
         Função filtra dados de menor temperatura do ano e sua data.
         """
-        menor_temp = self.max_int
+        menor_temperatura = self.max_int
         maior_data = None
 
         for i in range(len(self.df)):
-            if menor_temp > self.df.iloc[i]['Temp_Min']:
-                menor_temp = self.df.iloc[i]['Temp_Min']
+            if menor_temperatura > self.df.iloc[i]['Temp_Min']:
+                menor_temperatura = self.df.iloc[i]['Temp_Min']
                 maior_data = self.df.iloc[i]['Data']
 
-        resposta = f"O dia {maior_data} com menor temperatura teve um total de {menor_temp} ºC."
-        return resposta
+        return menor_temperatura, maior_data
 
     def obter_data_maior_precipatacao(self):
         """
         Função filtra dados de maior precipitação do ano e sua data.
         """
-        maior_preci = self.min_int
+        maior_precipatacao = self.min_int
         maior_data = None
 
         for i in range(len(self.df)):
-            if maior_preci < self.df.iloc[i]['Precipitacao']:
-                maior_preci = self.df.iloc[i]['Precipitacao']
+            if maior_precipatacao < self.df.iloc[i]['Precipitacao']:
+                maior_precipatacao = self.df.iloc[i]['Precipitacao']
                 maior_data = self.df.iloc[i]['Data']
 
-        resposta = f"O dia {maior_data} com mais chuvoso teve um total de {maior_preci} mm."
-        return resposta
+        return maior_precipatacao, maior_data
 
     def obter_data_maior_consumo(self):
         """
@@ -93,8 +98,7 @@ class DescritorTabela:
                 maior_consumo = self.df.iloc[i]['Consumo_em_litros']
                 maior_data = self.df.iloc[i]['Data']
 
-        resposta = f"O dia {maior_data} com mais consumo teve um total de {maior_consumo} litros."
-        return resposta
+        return maior_consumo, maior_data
 
     def obter_data_menor_consumo(self):
         """
@@ -108,8 +112,7 @@ class DescritorTabela:
                 menor_consumo = self.df.iloc[i]['Consumo_em_litros']
                 maior_data = self.df.iloc[i]['Data']
 
-        resposta = f"O dia {maior_data} com menor consumo teve um total de {menor_consumo} litros."
-        return resposta
+        return menor_consumo, maior_data
 
     def obter_media_consumo(self):
         """
@@ -117,8 +120,7 @@ class DescritorTabela:
         """
         coluna_media_consumo = self.df.Consumo_em_litros
         self.consumo = coluna_media_consumo.mean()
-        resposta = f"Média de consumo por dia foi de {self.consumo} litros consumidos."
-        return resposta
+        return self.consumo
 
     def obter_soma_consumo(self):
         """
@@ -129,6 +131,28 @@ class DescritorTabela:
         resposta = f"A soma de consumo do ano foi de {self.consumo} litros consumidos."
         return resposta
 
+    def df(self):
+        return self.df
 
-dt = DescritorTabela("C:\FrameworkTalitha\Consumo_cerveja2.tsv")
-print(dt.pesquisar_por_data())
+
+class DescritorLocal(DescritorTabela):
+
+    def __init__(self, caminho_arq=None):
+        super().__init__()
+
+        caminho_arq = "C:\FrameworkTalitha\dados\consumo_cerveja.tsv" if not caminho_arq else caminho_arq
+        self.df = pd.read_csv(caminho_arq, sep='\t')
+        self.preparar_df()
+
+
+class DescritorRemoto(DescritorTabela):
+
+    def __init__(self, url_base_dados=None):
+        super().__init__()
+        # URL dos dados
+        url_base_dados = "https://raw.githubusercontent.com/Dauerrr/FrameWork-Descritor-Analise-SP/main/dados/consumo_cerveja.tsv" if not url_base_dados else url_base_dados
+        conteudo = requests.get(url_base_dados).content
+        conteudo_utf8 = io.StringIO(conteudo.decode('utf-8'))
+        self.df = pd.read_csv(conteudo_utf8, sep='\t')
+        self.preparar_df()
+        
