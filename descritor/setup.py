abb4b863327dcd3ac80dac814f009516ec2c973a
Pedro Dauer <81972735+Dauerrr@users.noreply.github.com>
2021-12-03 15:31:27 -0300

    Add files via upload

---
+++ descritor/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='FrameWorkConsumo',
+    version='0.0.1',
+
+    url='https://github.com/Dauerrr/FrameworkConsumo',
+    author='Pedro Dauer',
+    author_email='pedro.dauer2003@gmail.com',
+
+    py_modules=['descritor'],
+    install_requires=[
+    'pandas',
+    'requests',
+    'sys',
+],
+)
